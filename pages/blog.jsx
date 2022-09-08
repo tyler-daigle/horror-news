f2b80d29753292efa3257b0742bd9f0ea5eec753
Tyler D <tyler@tdaigle.com>
2022-09-08 16:23:41 -0400

    basic styles and apollo client pulling data from the cms

---
+++ pages/blog.jsx
@@ -0,0 +1,8 @@
+export default function Blog() {
+  return (
+    <div>
+      <h1>Welcome to the blog!</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio dolor, reprehenderit quo iste quas quis?</p>
+    </div>
+  )
+}
